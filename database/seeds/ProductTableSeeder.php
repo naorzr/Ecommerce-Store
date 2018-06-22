eb677ffad7752a5603e67ab2e15cc3c2266f3f8c
Naor <naorzruk@gmail.com>
2018-06-22 18:06:46 +0300

    create a route for Productlist, completely changed the frontend layout. added jumbotron,navbar, fixed the routing for about and contant and removed the admin route

---
+++ database/seeds/ProductTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ProductTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for($x = 0;$x <=8 ; $x++){
+            $product = new \App\Product(
+                [
+                    'imagePath'=>'https://preview.ibb.co/khzTx8/20180620_232043.jpg',
+                    'title'=>'Harry Popper $x',
+                    'description'=> 'Well.. This is a description $x',
+                    'price' => 10.42 +$x
+                ]
+            );
+            $product->save();
+    }
+    }
+}
---
+++ resources/assets/js/components/Jumbotron.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="jumbotron">
+  <div class="container text-center">
+    <h1>T I F E R T</h1>      
+    <p>Beauty & Nail Art</p>
+    </div>
+  </div>
+</template>
